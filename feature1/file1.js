8cd2aa1094fc65c2fc9406be6e6c9a5a31cf0ab1
parasbheda <paras.bheda@studentuniverse.com>
2015-11-17 14:27:09 -0500

    changes for feature2 in feature 1 folder

--- feature1/file1.js
+++ feature1/file1.js
@@ -1,4 +1,8 @@
 //some code related to feature one
 console.log('TEST');
 
+<<<<<<< HEAD
 // some changes for hot-fix --compatibility issues
+=======
+//some changes for feature 2 comaptilbility
+>>>>>>> changes for feature2 in feature 1 folder
